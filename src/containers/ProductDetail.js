25bf877a7be5ca5e1eee7375f2db0a706bcc402b
Pawan Kumar <surpawan@gmail.com>
2021-07-04 20:27:50 +0530

    Added loading to product detail page.

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -8,7 +8,8 @@ import { selectedProduct } from "../redux/actions/productActions";
 const ProductDetail = () => {
   const dispatch = useDispatch();
   const { productId } = useParams();
-  const product = useSelector((state) => state.allProducts.selectedProduct);
+  const product = useSelector((state) => state.product);
+  const { image, title, price, category, description } = product;
 
   const fetchProductDetail = async () => {
     const response = await axios
@@ -23,8 +24,37 @@ const ProductDetail = () => {
   }, [productId]);
 
   return (
-    <div>
-      <h1>Product Detail</h1>
+    <div className="ui grid container">
+      {Object.keys(product).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <div className="ui placeholder segment">
+          <div className="ui two column stackable center aligned grid">
+            <div className="ui vertical divider">AND</div>
+            <div className="middle aligned row">
+              <div className="column lp">
+                <img className="ui fluid image" src={image} alt={title} />
+              </div>
+              <div className="column rp">
+                <h1>{title}</h1>
+                <h2>
+                  <a className="ui teal tag label" href="#">
+                    ${price}
+                  </a>
+                </h2>
+                <h3 className="ui brown block header">{category}</h3>
+                <p>{description}</p>
+                <div className="ui vertical animated button" tabIndex="0">
+                  <div className="hidden content">
+                    <i className="shop icon"></i>
+                  </div>
+                  <div className="visible content">Add to Cart</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
